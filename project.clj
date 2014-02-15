9698ea6645ef79d15e36e8b11e27ffb572946fff
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-02-15 19:27:05 +0800

    add annotations, before passes

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject org.clojure/tools.analyzer "0.1.0-SNAPSHOT"
+(defproject org.clojure/tools.analyzer "0.1.0-typed-SNAPSHOT"
   :description "An analyzer for Clojure code, written in Clojure and producing AST in EDN."
   :url "https://github.com/clojure/tools.analyzer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
+  :profiles {:dev {:repl-options {:port 64522}}}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.datomic/datomic-free "0.9.4384" :scope "provided"]])
+                 [org.clojure/core.typed "0.2.31"]
+                 #_[com.datomic/datomic-free "0.9.4384" :scope "provided"]])
